9f719cc824a2b793fa1848e5760de2f2303bb2f8
Agung Sudrajat Supriatna <agung.sudrajat@gmail.com>
2022-08-10 00:40:21 +0700

    http client PUT request

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
 
 import { Post } from '../../models/Post';
@@ -9,8 +9,11 @@ import { Post } from '../../models/Post';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
-  post: Post;
+
   @Output() newPost: EventEmitter<Post> = new EventEmitter();
+  @Output() updatedPost: EventEmitter<Post> =  new EventEmitter();
+  @Input() currentPost: Post;
+  @Input() isEdit: boolean;
 
 
   constructor(private postService : PostService) { }
@@ -26,6 +29,15 @@ export class PostFormComponent implements OnInit {
         this.newPost.emit(post);
       })
     }
+  }
+
+  updatePost() {
+    this.postService.updatePost(this.currentPost).subscribe
+    (post => {
+      console.log(post);
+      this.isEdit = false;
+      this.updatedPost.emit(post);
+    });
   }
 
 }
